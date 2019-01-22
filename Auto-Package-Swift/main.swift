35f0bb5d3d1cb384868639248fcdd8494eba1ff0
LuFP <lfp@cryptape.com>
2019-01-22 10:26:20 +0800

    Initialize commit

---
+++ Auto-Package-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Auto-Package-Swift
+//
+//  Created by XiaoLu on 2019/1/22.
+//  Copyright © 2019 xl. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
